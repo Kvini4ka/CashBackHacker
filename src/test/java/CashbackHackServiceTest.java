65fe48a9e4a2fc521704347d542d1e0ef9bb97cd
Kvini4ka <kvini4ka@mail.ru>
2023-06-21 17:42:10 +0300

    JUnit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,9 +1,7 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 import ru.netology.CashbackHackService;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
